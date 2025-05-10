27954278e7a9c372344df90da9d035e139a758cc
DHRUVGANGAL <dhruvgangal123@gmail.com>
2025-05-10 23:57:48 +0530

    fix error

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const adminRoutes = require("./Routes/admin.js");
 app.use(cors({
   origin: [ 
     "http://localhost:3000", 
-    "http://localhost:3001"  
+    "http://localhost:3001",
+    "https://healthily-backend.vercel.app",
+    "https://healthily-backend-git-main-dhruvgangals-projects.vercel.app"  
   ],
   methods: ["GET", "POST"],
   credentials: true
@@ -66,8 +68,11 @@ const server = app.listen(process.env.PORT || 8000, () => {
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:3000", "http://localhost:3001",
-      "https://healthily-backend-git-main-dhruvgangals-projects.vercel.app"
+      "http://localhost:3000", 
+      "http://localhost:3001",
+    "https://healthily-backend.vercel.app",
+    "https://healthily-backend-git-main-dhruvgangals-projects.vercel.app"
+      
     ],
     methods: ["GET", "POST"],
     credentials: true
